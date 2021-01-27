85b59d98349e0fefbfaa197c1134b4242e002662
Daniel Lugo <58088181+dalugoSU@users.noreply.github.com>
2021-01-27 00:46:35 -0500

    Create GUI_CONSTANTS.py

---
+++ GUI_CONSTANTS.py
@@ -0,0 +1,6 @@
+MINX, MINY = 500, 500
+MAXX, MAXY = 500, 500
+WINDOW_TITLE = "Daniel's Calculator"
+
+ADD, SUBTRACT, DIVIDE, MULTIPLY = " + ", " - ", " / ", " X "
+DOT, ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE = ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
